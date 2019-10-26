104672f3d66ef9ff82fd14a5c01cc41f092a2562
Gounane Abderrahim <gounane.abderrahim@gmail.com>
2019-10-26 10:55:52 +0100

    More tests

---
+++ tests/acceptance/HomeCest.php
@@ -0,0 +1,15 @@
+<?php 
+
+class HomeCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+    	$I->amOnPage('/');
+    	$I->see('Hello World');
+    }
+}
--- wp-content/themes/child-theme/functions.php
+++ wp-content/themes/child-theme/functions.php
@@ -5,20 +5,3 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
 
-
-add_action('wp_head', 'add_google_analytic_tag');
-function add_google_analytic_tag(){
-	$tag = <<<ANALYTICS
-		<script type="text/javascript">
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-XXXXXXXX']);
-  _gaq.push(['_trackPageview']);
-  (function() {
-   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })(); </script>;
-ANALYTICS;
-
-	echo $tag;
-}
